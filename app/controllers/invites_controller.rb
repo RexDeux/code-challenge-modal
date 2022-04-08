d477b97204d26c3c083c510d1ec9283aaec504e5
Nuno Costa <nm.cdc@hotmail.com>
2022-04-08 11:54:37 +0100

    fixed

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -4,20 +4,22 @@ class InvitesController < ApplicationController
     @cycle = Cycle.find(params[:cycle_id])
     respond_to do |format|
       format.html
-      format.json
+      format.js
     end
   end
 
   def create
     @invite = Invite.new
     @invite.cycle = Cycle.find(params[:cycle_id])
-    if @invite.save
-      redirect_to cycles_path
-    else
-      redirect_to cycles_path
+    respond_to do |format|
+      #added notices in case the post request failed
+      if @invite.save
+        format.html { redirect_to root_path, flash: {notice: "Invite successfully sent!" }}
+      else
+        format.html { redirect_to root_path, flash: {alert: "Invite failed!" }}
+      end
     end
   end
-
   private
 
   def invite_params
