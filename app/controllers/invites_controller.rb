dd2ecdbf2c03b1352759fcecfd91b4e721f09d46
Nuno Costa <nm.cdc@hotmail.com>
2022-04-08 14:53:54 +0100

    more comments «

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,7 +1,7 @@
 class InvitesController < ApplicationController
   def new
-    @invite = Invite.new
-    @cycle = Cycle.find(params[:cycle_id])
+    @invite = Invite.new #initiate the instance
+    @cycle = Cycle.find(params[:cycle_id]) #attribute cycle_id to the cycle
     respond_to do |format|
       format.html
       format.js
@@ -9,15 +9,15 @@ class InvitesController < ApplicationController
   end
 
   def create
-    @invite = Invite.new
-    @invite.cycle = Cycle.find(params[:cycle_id])
+    @invite = Invite.new #initiate the instance
+    @invite.cycle = Cycle.find(params[:cycle_id]) #interconnect the invite with each cycle and make cycle_id the params
     respond_to do |format|
       #added notices in case the post request failed
       if @invite.save
-        format.html { redirect_to root_path, flash: {notice: "Invite successfully sent!" }}
+        format.html { redirect_to root_path, flash: {notice: "Invite successfully sent!" }} #redirects to root and provides a notice
       else
         format.html { redirect_to root_path, flash: {alert: "Invite failed!" }}
-        format.js { render :action => 'new' }
+        format.js { render :action => 'new' } #for the stimulus js
       end
     end
   end
@@ -25,6 +25,6 @@ class InvitesController < ApplicationController
   private
 
   def invite_params
-    params.require(:invites).permit(:email, :cycle_id, :message)
+    params.require(:invites).permit(:email, :cycle_id, :message) #cycle_id seems to be unnecessary but I kept it
   end
 end
